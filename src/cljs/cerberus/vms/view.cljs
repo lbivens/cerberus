ec4ac4a953d9d6e9d989341035afc6f143967837
Heinz N. Gies <heinz@licenser.net>
2015-08-28 21:28:53 +0200

    Azis and styling for graphs

--- src/cljs/cerberus/vms/view.cljs
+++ src/cljs/cerberus/vms/view.cljs
@@ -651,9 +651,11 @@
     [name points]
     [name (map #(* (/ (- max  %) max) 100) points)]))
 
+(defn mkp [points]
+  (apply str (map (fn [[x y]] (str x "," y " ")) points)))
 
 (defn point-view
-  [[name lines] owner]
+  [{name :name lines :lines max :max} owner]
   (reify
     om/IRender
     (render [this]
@@ -661,34 +663,36 @@
        {:xs 12 :sm 6 :md 4 :lg 3
         :style {:text-align "center"}}
        (p/panel
-        {:header name}
-        (d/svg
-         {:width  "300px"
-          :height "160px"
-          :viewBox "0 0 180 100"
-          :style { :background "none" } }
-         (let [max (apply max (map max-metric lines))]
+        {:header name
+         :class  name}
+        (let [x 20
+              y 10]
+          (d/svg
+           {:class   "omg"
+            :viewBox "0 0 200 100"}
+           ;; max text
+           (d/text {:x 0 :y 0 :class "label max"} (Math/round max))
+           ;; min text
+           (d/text {:x 10 :y 100 :class "label min"} 0)
+           ;; x-line
            (d/polyline
-             {:key 0
-              :points (str "0,0 0,100")
-              :style {:stroke       "red"; "#95D4EC"
-                      :stroke-width "1"
-                      :fill         "none"}}))
-         (d/polyline
-          {:key 1
-           :points (str "120,100 0,100")
-           :style {:stroke       "red";"#95D4EC"
-                   :stroke-width "1"
-                   :fill         "none"}})
-         (map
-          (fn [[line points]]
-            (d/polyline
-             {:key line
-              :points (apply str (map-indexed (fn [a b] (str (* a 2) "," b " ")) points))
-              :style {:stroke       "#95D4EC"
-                      :stroke-width "1"
-                      :fill         "none"}}))
-          lines)))))))
+            {:key 1
+             :points (mkp [[(- x 4) (- 101 y)] [(+ 120 x) (- 101 y)]])
+             :class "axis x"})
+           ;;y - line
+           (d/polyline
+            {:key 0
+             :points (mkp [[(- x 1) (* -1 y)] [(- x 1) (- 100 (/ y 2))]])
+             :class "axis y"})
+           (map-indexed
+            (fn [idx [line points]]
+              (d/polyline
+               {:key line
+                :points (mkp (map-indexed (fn [a b] [(+ (* a 2) x) (- b 10)]) points))
+                :class  (str "graph graph-" line " graph-" idx)
+                :style {:fill "none"}}
+               ))
+            lines))))))))
 
 
 (defn process-metric [{name :n points :v}]
@@ -725,7 +729,9 @@
 
 (defn normalize-metrics [[name metrics]]
   (let [max (apply max (map max-metric metrics))]
-    [name  (map (partial normalize-metric max) metrics)]))
+    {:name name
+     :max max
+     :lines (map (partial normalize-metric max) metrics)}))
 
 (defn render-metrics [data owner opts]
   (reify

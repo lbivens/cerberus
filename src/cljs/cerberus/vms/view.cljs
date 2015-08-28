a16b838ba5cd206457ae80f717292d01ab59d89d
Heinz N. Gies <heinz@licenser.net>
2015-08-28 21:45:40 +0200

    Some cleanup for graphs

--- src/cljs/cerberus/vms/view.cljs
+++ src/cljs/cerberus/vms/view.cljs
@@ -654,9 +654,45 @@
 (defn mkp [points]
   (apply str (map (fn [[x y]] (str x "," y " ")) points)))
 
-(defn point-view
+
+(defn omg
   [{name :name lines :lines max :max} owner]
   (reify
+    om/IDisplayName
+    (display-name [_]
+        "OMG")
+    om/IRender
+    (render [this]
+      (let [x 20
+            y 10]
+        (d/svg
+         {:class   "omg"
+          :viewBox "0 0 200 100"}
+         ;; max text
+         (d/text {:x 0 :y 0 :class "label max"} (Math/round max))
+         ;; min text
+         (d/text {:x 10 :y 100 :class "label min"} 0)
+         ;; x-line
+         (d/polyline
+          {:points (mkp [[(- x 4) (- 101 y)] [(+ 120 x) (- 101 y)]])
+           :class "axis x"})
+         ;;y - line
+         (d/polyline
+          {:points (mkp [[(- x 1) (* -1 y)] [(- x 1) (- 100 (/ y 2))]])
+           :class "axis y"})
+         (map-indexed
+          (fn [idx [line points]]
+            (d/polyline
+             {:points (mkp (map-indexed (fn [a b] [(+ (* a 2) x) (- b 10)]) points))
+              :class  (str "line line-" line " line-" idx)
+              :style {:fill "none"}}))
+          lines))))))
+
+(defn point-view [{name :name :as data} owner]
+  (reify
+    om/IDisplayName
+    (display-name [_]
+      "point-view")
     om/IRender
     (render [this]
       (g/col
@@ -665,35 +701,7 @@
        (p/panel
         {:header name
          :class  name}
-        (let [x 20
-              y 10]
-          (d/svg
-           {:class   "omg"
-            :viewBox "0 0 200 100"}
-           ;; max text
-           (d/text {:x 0 :y 0 :class "label max"} (Math/round max))
-           ;; min text
-           (d/text {:x 10 :y 100 :class "label min"} 0)
-           ;; x-line
-           (d/polyline
-            {:key 1
-             :points (mkp [[(- x 4) (- 101 y)] [(+ 120 x) (- 101 y)]])
-             :class "axis x"})
-           ;;y - line
-           (d/polyline
-            {:key 0
-             :points (mkp [[(- x 1) (* -1 y)] [(- x 1) (- 100 (/ y 2))]])
-             :class "axis y"})
-           (map-indexed
-            (fn [idx [line points]]
-              (d/polyline
-               {:key line
-                :points (mkp (map-indexed (fn [a b] [(+ (* a 2) x) (- b 10)]) points))
-                :class  (str "graph graph-" line " graph-" idx)
-                :style {:fill "none"}}
-               ))
-            lines))))))))
-
+        (om/build omg data))))))
 
 (defn process-metric [{name :n points :v}]
   {:name (clojure.string/split name #"-")

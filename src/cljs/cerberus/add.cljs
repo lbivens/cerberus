lbivens/cerberus

commit b479b0e493759f0a6725ae3a80da48a47d764a13

Text for 'add button'

src/cljs/cerberus/add.cljs
@@ -47,6 +47,18 @@
    :dtrace   "Create DTrace Script"
    :datasets "Import Dataset"})
 
+(def submit-text
+  {:vms      "Create"
+   :users    "Add User"
+   :roles    "Add Role"
+   :orgs     "Add Organisation"
+   :clients  "add Client"
+   :packages "Add Package"
+   :networks "Add Network"
+   :ipranges "Add IP-Range"
+   :dtrace   "Add DTrace Script"
+   :datasets "Import Dataset"})
+
 (def add-submit
   {:datasets datasets/submit})
 
@@ -118,16 +130,17 @@
              (g/col
               {:md 12 :style {:text-align "center"}}
               (d/h4
-                    (add-title section)
-                    (b/toolbar {}
-                      (b/button { :bs-style "primary" 
-                                  :class (if (get-in data [:content :valid])
-                                           "pull-right createbutton valid"
-                                           "pull-right createbutton invalid")
-                                  :disabled? (if (get-in data [:content :valid])
-                                           false
-                                           true)          
-                                  :on-click #(submit-add data)} "Start Process"))))))
+               (add-title section)
+               (b/toolbar
+                {}
+                (b/button
+                 {:bs-style "primary"
+                  :class (str "pull-right createbutton"
+                              (if (get-in data [:content :valid])
+                                " valid"
+                                " invalid"))
+                  :disabled? (get-in data [:content :valid])
+                  :on-click #(submit-add data)} (submit-text section)))))))
           (g/row
            {:id "add-content"}
            (if-let [create-view (add-renderer section)]

src/cljs/cerberus/users/view.cljs
@@ -30,36 +30,38 @@
     (p/panel
      {:header (d/h3 "Change Password")}
      (d/form
+      (i/input
+       {:type "password" :label "New Password"
+        :id "changepass1"
+        :value (:password1-val state)
+        :on-change  #(validate/match
+                      %
+                      (val-by-id  "changepass2")
+                      :password-validate
+                      :password1-val
+                      owner)})
 
-      (i/input {:type "password" :label "New Password"
-                :id "changepass1"
-                :value (:password1-val state)
-                :on-change  #(validate/match
-                              %
-                              (val-by-id  "changepass2")
-                              :password-validate
-                              :password1-val
-                              owner)})
-
-      (i/input {:type "password" :label "Confirm"
-                :id "changepass2"
-                :value (:password2-val state)
-                :bs-style (if (or (:password-validate state)
-                                  (blank? (:password2-val state)))
-                            nil "error")
-                :on-change  #(validate/match
-                              %
-                              (val-by-id  "changepass1")
-                              :password-validate
-                              :password2-val
-                              owner)})
-      (b/button {:bs-style "primary"
-                 :className "pull-right"
-                 :onClick #(do
-                             (alert/raise :success "Password changed")
-                             (users/changepass uuid (:password1-val state)))
-                 :disabled? (false? (:password-validate state))}
-                "Change")))))
+      (i/input
+       {:type "password" :label "Confirm"
+        :id "changepass2"
+        :value (:password2-val state)
+        :bs-style (if (or (:password-validate state)
+                          (blank? (:password2-val state)))
+                    nil "error")
+        :on-change  #(validate/match
+                      %
+                      (val-by-id  "changepass1")
+                      :password-validate
+                      :password2-val
+                      owner)})
+      (b/button
+       {:bs-style "primary"
+        :className "pull-right"
+        :onClick #(do
+                    (alert/raise :success "Password changed")
+                    (users/changepass uuid (:password1-val state)))
+        :disabled? (false? (:password-validate state))}
+       "Change")))))
 
 (defn clean-key [owner]
   (om/set-state! owner :key-name-value "")
@@ -94,18 +96,7 @@
                          :on-click #(submit-key uuid owner state)}
                         "Add"))}
     (d/form
-     (i/input
-      {:type "text" :label "Name"
-       :id "newsshkeyname"
-       :value (:key-name-value state)
-       :on-change
-       #(do
-          (om/set-state! owner :ssh-key-name-edited true)
-          (validate/nonempty
-           %
-           :key-name-validate
-           :key-name-value
-           owner))})
+
      (i/input
       {:type "textarea" :label "Key"
        :id "newsshkey"
@@ -118,10 +109,22 @@
              owner :key-name-value
              (last (clojure.string/split (val-by-id "newsshkey") #" "))))
           (validate/nonempty
-               %
-               :key-data-validate
-               :key-data-value
-               owner))})))))
+           %
+           :key-data-validate
+           :key-data-value
+           owner))})
+     (i/input
+      {:type "text" :label "Name"
+       :id "newsshkeyname"
+       :value (:key-name-value state)
+       :on-change
+       #(do
+          (om/set-state! owner :ssh-key-name-edited true)
+          (validate/nonempty
+           %
+           :key-name-validate
+           :key-name-value
+           owner))})))))
 
 (defn ssh-key-li [uuid key-name key-data]
   (d/li

commit e2de23ccfae048fb987501716c330cc6165eab55

re-fix button non-clickable logic broken by commit b479b0e493759f0a6725ae3a80da48a47d764a13

src/cljs/cerberus/add.cljs
@@ -133,17 +133,16 @@
              (g/col
               {:md 12 :style {:text-align "center"}}
               (d/h4
-               (add-title section)
-               (b/toolbar
-                {}
-                (b/button
-                 {:bs-style "primary"
-                  :class (str "createbutton"
-                              (if (get-in data [:content :valid])
-                                " valid"
-                                " invalid"))
-                  :disabled? (get-in data [:content :valid])
-                  :on-click #(submit-add data)} (submit-text section)))))))
+                    (add-title section)
+                    (b/toolbar {}
+                      (b/button { :bs-style "primary" 
+                                  :class (if (get-in data [:content :valid])
+                                           "pull-right createbutton valid"
+                                           "pull-right createbutton invalid")
+                                  :disabled? (if (get-in data [:content :valid])
+                                           false
+                                           true)          
+                                  :on-click #(submit-add data)} (submit-text section)))))))
           (g/row
            {:id "add-content"}
            (if-let [create-view (add-renderer section)]

src/less/style.less
@@ -1221,9 +1221,14 @@ div.filterbar .btn-group .dropdown-menu li a:hover{
 }
 
 #add-view #add-hdr button.createbutton {
+    background-color: #48b4de;
     margin-right: 10px;
     margin-bottom: 15px;
     float: right;
+}
+
+#add-view #add-hdr button.createbutton:hover {
+    background-color: #32abda;
 }
 
 #add-view span#add-plus-btn {
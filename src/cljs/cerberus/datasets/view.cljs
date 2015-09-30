e790da3a82be9a71206b770b47f39e142e89b114
Heinz N. Gies <heinz@licenser.net>
2015-09-30 02:29:22 +0200

    Allow adding and removing nics from a dataset

--- src/cljs/cerberus/datasets/view.cljs
+++ src/cljs/cerberus/datasets/view.cljs
@@ -8,7 +8,8 @@
    [om-bootstrap.random :as r]
    [om-bootstrap.nav :as n]
    [om-bootstrap.input :as i]
-   [cerberus.utils :refer [goto grid-row display]]
+   [om-bootstrap.button :as b]
+   [cerberus.utils :refer [goto ->state grid-row display]]
    [cerberus.http :as http]
    [cerberus.api :as api]
    [cerberus.datasets.api :refer [root] :as datasets]
@@ -42,21 +43,47 @@
        (d/ul
         (map render-requirement app))))))
 
-(defn networks [app element]
+(defn next-if [app]
+  (loop [i 0]
+    (if (some #(= (str "net" i) (:name %)) app)
+      (recur (inc i))
+      (str "net" i))))
+
+(defn networks [{:keys [networks uuid]} owner]
   (reify
     om/IRenderState
-    (render-state [_ _]
+    (render-state [_ {desc :desc}]
       (r/well
        {}
-       (d/dl
-        (map (fn [{desc :description name :name}]
-               [(d/dt name) (d/dd desc)]) app))))))
-
-
+       (let [iface (next-if networks)]
+         (g/row
+          {}
+          (g/col
+           {:xs 10 :sm 5}
+           (i/input {:addon-before iface
+                     :type "input" :value desc
+                     :placeholder "Description"
+                     :on-change (->state owner :desc)}))
+          (g/col
+           {:xs 2 :sm 1}
+           (b/button {:bs-style "success"
+                      :disabled? (or (nil? desc) (empty? desc))
+                      :on-click #(datasets/add-nic uuid iface desc)} "Add"))
+          (g/col
+           {:xs 12 :sm 6}
+           (d/dl
+            (map (fn [{desc :description name :name}]
+                   [
+                    (d/dt name
+                          (b/button {:bs-style "danger" :bs-size "xsmall"
+                                     :class "pull-right"
+                                     :on-click #(datasets/delete-nic uuid name)}
+                                    (r/glyphicon {:glyph "trash"})))
+                    (d/dd desc)]) networks)))))))))
 
 (def sections {""              {:key  1 :fn #(om/build render-home %2)      :title "General"}
                "requirements"  {:key  2 :fn #(om/build requirements (:requirements %2))     :title "Requirements"}
-               "networks"      {:key  3 :fn #(om/build networks (:networks %2))         :title "Networks"}
+               "networks"      {:key  3 :fn #(om/build networks %2)         :title "Networks"}
                "metadata"      {:key  4 :fn #(om/build metadata/render %2)  :title "Metadata"}})
 
 (def render

7952f95ca465bad78ceface39c29242750516119
Heinz N. Gies <heinz@licenser.net>
2016-12-15 16:15:41 +0100

    Update pkg.mk

--- rel/pkg/pkg.mk
+++ rel/pkg/pkg.mk
@@ -9,9 +9,9 @@ PKG_HOMEPAGE ?=https://project-fifo.net
 .PHONY: package_list build_info clean-pkg
 
 package_list:
-	-rm packlist
+	-rm packlist || true
 	for dep in $(DEPS); do echo "@pkgdep $$dep" >> packlist; done
-	(cd deploy; find * -type f | sort) >> packlist
+	(cd $(STAGE_DIR); find * -type f | sort) >> packlist
 
 build_info:
 	pkg_info -X pkg_install | egrep '^(MACHINE_ARCH|OPSYS|OS_VERSION|PKGTOOLS_VERSION)' >build-info
@@ -43,11 +43,10 @@ endif
 	split -b $(BLOCK_SIZE) tmp/$(FILE).tgz tmp/split
 	echo "pkgsrc signature\n\nversion: 1\npkgname: $(FILE)" > +PKG_HASH
 	echo "algorithm: SHA512\nblock size: $(BLOCK_SIZE)" >> +PKG_HASH
-	echo "file size: $(shell ls -l tmp/$(FILE).tgz | cut -d' ' -f 5)\n" >> +PKG_HASH
+	echo "file size: $(shell ls -l tmp/$(FILE).tgz | awk '{print $5;}')\n" >> +PKG_HASH
 	for file in tmp/split*; do digest -a sha512 $$file >> +PKG_HASH; done
 	echo "end pkgsrc signature" >> +PKG_HASH
 
 +PKG_GPG_SIGNATURE: +PKG_HASH
 	-rm +PKG_GPG_SIGNATURE
 	gpg --default-key $(GPG_KEY) -o +PKG_GPG_SIGNATURE --detach-sign --armor +PKG_HASH
-
